5001259b25ef8bbbf1ec3cbbdd9fc65fa0e362b2
mihaiishere <mihai_andrei_gherghel@yahoo.com>
2022-01-06 03:09:32 +0200

    DFS CONTRACT GENERATOR

---
+++ src/licenta/Licenta.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package licenta;
+
+import LoginSystems.LoginSystem;
+import PrintSystems.GeneratorContracte;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author mihai
+ */
+public class Licenta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //new GeneratorContracte().setVisible(true);
+        new LoginSystem().setVisible(true);
+    }
+    
+}
